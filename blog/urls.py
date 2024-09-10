6e9ce2836ac78ad1d6ba3db450dc5cc0f9c10ea8
Alina Baranova <alina_nemo@mail.ru>
2024-09-10 15:47:44 +0300

    Update

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView
+from blog.views import BlogCreateView, BlogUpdateView, BlogListView, BlogDetailView, BlogDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('list/', BlogListView.as_view(), name='blog_list'),
+    path('view/<slug:the_slug_blog>/', BlogDetailView.as_view(), name='blog_view'),
+    path('update/<slug:the_slug_blog>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('delete/<slug:the_slug_blog>/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,41 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.text import slugify
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import Blog
 
 
-# class BlogListView(ListView):
-#     model = Blog
-# class BlogDetailView(DetailView):
-
 class BlogCreateView(CreateView):
     model = Blog
     fields = ['title', 'content', 'preview', 'published']
-    success_url = reverse_lazy('catalog:product_list')
-# class BlogUpdateView(UpdateView):
-#
-# class BlogDeleteView(DeleteView):
+    success_url = reverse_lazy('blog:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+
+class BlogListView(ListView):
+    model = Blog
+    slug_url_kwarg = 'the_slug_blog'
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    slug_url_kwarg = 'the_slug_blog'
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'content', 'preview', 'published']
+    success_url = reverse_lazy('blog:blog_list')
+    slug_url_kwarg = 'the_slug_blog'
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:blog_list')
+    slug_url_kwarg = 'the_slug_blog'
