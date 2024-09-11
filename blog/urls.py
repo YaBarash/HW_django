5b8ec38936b8d28165d8c2ebccffc499cf307101
Alina Baranova <alina_nemo@mail.ru>
2024-09-11 09:58:07 +0300

    Delete photo useless

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='blog_create'),
     path('list/', BlogListView.as_view(), name='blog_list'),
-    path('view/<slug:the_slug_blog>/', BlogDetailView.as_view(), name='blog_view'),
-    path('update/<slug:the_slug_blog>/', BlogUpdateView.as_view(), name='blog_update'),
-    path('delete/<slug:the_slug_blog>/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='blog_view'),
+    path('update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
