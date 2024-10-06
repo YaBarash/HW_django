9ebbd333f47e016f0ecf59ba1c4eeb5c549df006
Alina Baranova <alina_nemo@mail.ru>
2024-10-06 10:43:50 +0300

    Create model clients and make migrations

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email',)
+
+
+@admin.register
+class MailingMessageAdmin(admin.ModelAdmin):
+    list_display = ('client', 'sent_at',)
+    ordering = ('owner', ' letter_subject',)
+
+
+@admin.register
+class EmailSettingsAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'stop_time', 'status',)
+
+
+@admin.register
+class MailLogAdmin(admin.ModelAdmin):
+    list_display = ('try_status', 'last_attempt',)
